5ccd0252c9f5dd2947570e327bee3350afcae375
Shardul Silswal <sshardul007@gmail.com>
2023-08-22 23:03:53 +0530

    Moved map image src generation logic to class

--- app/components/map.js
+++ app/components/map.js
@@ -1,8 +1,20 @@
 import Component from '@glimmer/component';
 import ENV from 'super-rentals/config/environment';
 
+const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
+
 export default class MapComponent extends Component {
   get token() {
     return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
+  }
+
+  get src() {
+    let { lng, lat, zoom, width, height } = this.args;
+
+    let coordinates = `${lng},${lat},${zoom}`;
+    let dimensions = `${width}x${height}`;
+    let accesToken = `access_token=${this.token}`;
+
+    return `${MAPBOX_API}/${coordinates}/${dimensions}@2x?${accesToken}`;
   }
 }
