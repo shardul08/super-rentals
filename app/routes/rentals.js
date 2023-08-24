192fd929bb0433bbed77fad96ddd88e35f0b4cec
Shardul Silswal <sshardul007@gmail.com>
2023-08-25 00:23:21 +0530

    Add a dynamic route for each rental rentals/rental_id

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -2,13 +2,13 @@ import Route from '@ember/routing/route';
 
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 
-export default class IndexRoute extends Route {
-  async model() {
-    let response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
+export default class RentalsRoute extends Route {
+  async model(params) {
+    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
+    let { id, data } = await response.json();
 
     return data.map((model) => {
-      let { id, attributes } = model;
+      let { attributes } = model;
       let type;
 
       if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
