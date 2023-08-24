1e6d8c6110f7ae8eae1066406822d74c90e4ba85
Shardul Silswal <sshardul007@gmail.com>
2023-08-25 01:17:22 +0530

    Added rental template and detailed component for displaying rental in new page

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -5,19 +5,17 @@ const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 export default class RentalsRoute extends Route {
   async model(params) {
     let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { id, data } = await response.json();
+    let { data } = await response.json();
 
-    return data.map((model) => {
-      let { attributes } = model;
-      let type;
+    let { id, attributes } = data;
+    let type;
 
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
+    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+      type = 'Community';
+    } else {
+      type = 'Standalone';
+    }
 
-      return { id, type, ...attributes };
-    });
+    return { id, type, ...attributes };
   }
 }
